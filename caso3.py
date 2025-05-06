eb5ef75fd0a04e4126bb9e05d0d4f6d55b79648a
MarianaOrtegaRam <m.ortegar@uniandes.edu.co>
2025-05-06 18:47:48 -0500

    intento base

--- caso3.py
+++ caso3.py
@@ -1,46 +1,43 @@
 from geopy.distance import geodesic
 import pandas as pd
-from pyomo.environ import *
-from pyomo.opt import SolverFactory
 
-#----- procesamiento de datos/ encontrando las distancias entre todas las locations -----
-# https://geopy.readthedocs.io/en/stable/#module-geopy.distance 
-
+# --- 1. Carga de archivos del Caso 3 ---
 clientes = pd.read_csv('Proyecto_C_Caso3/clients.csv')
 depositos = pd.read_csv('Proyecto_C_Caso3/depots.csv')
 estaciones = pd.read_csv('Proyecto_C_Caso3/stations.csv')
 vehiculos = pd.read_csv('Proyecto_C_Caso3/vehicles.csv')
+peajes = pd.read_csv('Proyecto_C_Caso3/tolls.csv')  # <-- peajes para caso 3
 
-df = pd.read_csv('Proyecto_Caso_Base/locations.csv')
-# tomamos el mismo origen y las mismas primeras 15 localidades que en el caso base
-df = df[df['LocationID'] < 16]
+# --- 2. Crear archivo locations.csv ---
+# Empezamos con los mismos 15 nodos del caso base
+df_base = pd.read_csv('Proyecto_Caso_Base/locations.csv')
+df_base = df_base[df_base['LocationID'] < 16]  # PTO (1) + 14 municipios (2-15)
+df_base.to_csv('Proyecto_C_Caso3/locations.csv', index=False)
 
-df.to_csv('Proyecto_C_Caso3/locations.csv', index=False)
-
+# Agregar las estaciones al archivo de locations
 for i in range(len(estaciones)):
-    data= {'LocationID': estaciones['LocationID'][i],
-            'Longitude': estaciones['Longitude'][i],
-            'Latitude': estaciones['Latitude'][i],}
-    data_f = pd.DataFrame([data])
-    # modo a es de append pa ir añadiendo y como ya tiene encabezados header se pone en False
-    data_f.to_csv('Proyecto_C_Caso3/locations.csv', mode='a', header=False, index=False)
+    data = {
+        'LocationID': estaciones.loc[i, 'LocationID'],
+        'Longitude': estaciones.loc[i, 'Longitude'],
+        'Latitude': estaciones.loc[i, 'Latitude']
+    }
+    df_station = pd.DataFrame([data])
+    df_station.to_csv('Proyecto_C_Caso3/locations.csv', mode='a', header=False, index=False)
 
+# Leer locations.csv actualizado
 locations_csv = pd.read_csv('Proyecto_C_Caso3/locations.csv')
 
-locations = []
+# --- 3. Calcular matriz de distancias (en kilómetros) ---
+distancias = []
 for i in range(len(locations_csv)):
-    latitud = locations_csv['Latitude'][i]
-    longitud = locations_csv['Longitude'][i]
-    coord = (latitud, longitud)
-    locs_i=[]
-    j=0
-    while j<len(locations_csv):
-        latitud2 = locations_csv['Latitude'][j]
-        longitud2 = locations_csv['Longitude'][j]
-        coord2 = (latitud2, longitud2)
+    coord_i = (locations_csv.loc[i, 'Latitude'], locations_csv.loc[i, 'Longitude'])
+    fila = []
+    for j in range(len(locations_csv)):
+        coord_j = (locations_csv.loc[j, 'Latitude'], locations_csv.loc[j, 'Longitude'])
+        distancia = geodesic(coord_i, coord_j).kilometers
+        fila.append(distancia)
+    distancias.append(fila)
 
-        locs_i.append(geodesic(coord, coord2).meters)
-        j+=1
-    locations.append(locs_i)
-
-print (locations)
+# --- 4. Verificación ---
+print(f"Total de localidades cargadas: {len(locations_csv)}")
+print("Distancia entre nodo 1 y nodo 2:", distancias[0][1])
